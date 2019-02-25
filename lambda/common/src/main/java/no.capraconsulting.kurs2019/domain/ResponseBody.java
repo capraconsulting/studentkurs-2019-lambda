084113cdd911f26f73d0c13576b9496114871685
Oyvind Moldestad <moldestad@gmail.com>
2019-02-25 11:19:26 +0100

    Fix cors

--- lambda/common/src/main/java/no.capraconsulting.kurs2019/domain/ResponseBody.java
+++ lambda/common/src/main/java/no.capraconsulting.kurs2019/domain/ResponseBody.java
@@ -1,16 +1,30 @@
 package no.capraconsulting.kurs2019.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResponseBody {
 
     private int statusCode;
+    private Map<String, String> headers;
     private String body;
 
     public ResponseBody(int statusCode, String body) {
         this.statusCode = statusCode;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Headers", "*");
+        headers.put("Access-Control-Allow-Methods", "*");
+        this.headers = headers;
         this.body = body;
     }
 
     public ResponseBody(int statusCode) {
         this.statusCode = statusCode;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Headers", "*");
+        headers.put("Access-Control-Allow-Methods", "*");
+        this.headers = headers;
     }
 }
