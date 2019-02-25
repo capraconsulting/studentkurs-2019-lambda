83d21f685325a1094d927f8659685df87ddd4aa6
Oyvind Moldestad <moldestad@gmail.com>
2019-02-25 09:14:43 +0100

    Update SQL script

--- db/setup.sql
+++ db/setup.sql
@@ -1,6 +1,11 @@
-CREATE TABLE public."events" (
-  id           SERIAL PRIMARY KEY,
+CREATE TABLE events (
+  id           TEXT PRIMARY KEY,
   data         JSONB
 );
 
-ALTER TABLE public."events" OWNER TO eventsapp;
+ALTER TABLE events OWNER TO eventsapp;
+
+INSERT INTO events VALUES (
+  '783dc091-975c-418c-8e78-2db3bbcef15f',
+  '{"title": "Kurs med Capra","date": "2019-02-25T08:10:00.500Z","description": "Kurs for å sette opp en app med bruk av AWS","id": "783dc091-975c-418c-8e78-2db3bbcef15f"}'
+);
